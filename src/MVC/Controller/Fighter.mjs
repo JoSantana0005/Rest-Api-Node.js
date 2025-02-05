8b106096b5844032f67176f8f62c52ee9dff1795
JoSantana0005 <joseasantana05@gmail.com>
2025-02-05 10:30:43 -0400

    Add data in json

--- src/MVC/Controller/Fighter.mjs
+++ src/MVC/Controller/Fighter.mjs
@@ -1,5 +1,10 @@
 import { FighterModel } from "../Models/Fighter.mjs";
 
 export class FighterController{
+    constructor({fighterModel}){
+        this.fighterModel = fighterModel;
+    }
 
+    // Funcion para obtener todos los luchadores
+    
 }
--- src/MVC/Models/Fighter.mjs
+++ src/MVC/Models/Fighter.mjs
@@ -1,4 +1,68 @@
+import { READJSON } from "../Utils.mjs";
+const Fighters = READJSON("./Fighter.json");
 
 export class FighterModel{
+    // Funcion para obtner todos los luchadores
+    static async getALL(){
+        return Fighters;
+    }
+    // Obtener un luchador por su ID
+    static async getByID({id}){
+        if(id){
+            return Fighters.find(fighter => fighter.id == parseInt(id));
+        }
+        return Fighters;
+    }
 
+    // Obtener a lucahadores segun su categoria de peso
+    static async getByWeight({weight}){
+        if(weight){
+            return Fighters.filter(fighter => 
+                fighter.weight.includes(weight)
+            )
+        }
+        return Fighters;
+    }
+
+    // Obtener a un luchador por su nombre
+    static async getByName({name}){
+        if(name){
+            return Fighters.find(fighter => fighter.name.toLowerCase() == name.toLowerCase());
+        }
+        return Fighters;
+    }
+
+    // Crear un nuevo luchador
+
+    static async create({fighter}){
+        if(fighter){
+            const newFighter = {
+                id: Fighters.length + 1,
+                ...fighter
+            }
+            Fighters.push(newFighter);
+            return newFighter;
+        }
+        else{
+            return {error: "No se ha enviado un luchador"}
+        }
+    }
+
+    // Eliminar a un luchador por su id
+    static async delete({id}){
+        if(id){
+            const index = Fighters.findIndex(fighter =>
+                fighter.id == parseInt(id)
+            )
+            if(index !== -1){
+                Fighters.splice(index,1);
+                return {message: "Luchador eliminado"}
+            }
+            else{
+                return {error: "Luchador no encontrado"}
+            }
+        }else{
+            return {error: "No se ha enviado un id"}
+        }
+    }
 }
