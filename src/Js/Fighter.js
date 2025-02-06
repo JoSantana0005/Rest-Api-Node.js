2eae1573eb032552bf56c60d3bfbbfec6c15d526
JoSantana0005 <joseasantana05@gmail.com>
2025-02-06 13:09:47 -0400

    I almost There Fighter

--- src/Js/Fighter.js
+++ src/Js/Fighter.js
@@ -1,3 +1,21 @@
+const Ventana__modal = document.getElementById('Ventana--Modal');
+const Cerrar__Modal = document.getElementById('Close');
+const Name = document.getElementById('Title');
+const Plot = document.getElementById('Plot');
+const Age = document.getElementById('Age');
+const record = document.getElementById('Record');
+const Championship = document.getElementById('Championship');
+const Weight = document.getElementById('Weight');
+const Poster = document.getElementById('Imagen--Fighter')
+
+
+Cerrar__Modal.addEventListener('click',Cerrar_ventana);
+
+// Function para cerrar la ventana modal
+function Cerrar_ventana(){
+    Ventana__modal.close();
+}
+
 // Funcion para llamar la api
 async function ObtenerFighter(){
     try{
@@ -15,6 +33,25 @@ async function ObtenerFighter(){
                 `
             })
             document.getElementsByClassName('List--Fighter')[0].innerHTML = content;
+            // Abrir la ventana Modal
+            const Fighter = document.querySelectorAll('.Fighter');
+            if(Fighter){
+                Fighter.forEach((element) =>{
+                    element.addEventListener('click',()=>{
+                        if(Ventana__modal){
+                            Ventana__modal.showModal();
+                            const index = element.getAttribute('data-index');
+                            Name.textContent = Fighters[index].name;
+                            Plot.textContent = Fighters[index].plot;
+                            Age.textContent = Fighters[index].age;
+                            record.textContent = Fighters[index].record;
+                            Championship.textContent = Fighters[index].championship;
+                            Weight.textContent = Fighters[index].weight;
+                            Poster.src = Fighters[index].Poster;
+                        }
+                    })
+                })
+            }
         }
     }catch(error){
         console.error(`Hubo un error: ${error}`);
